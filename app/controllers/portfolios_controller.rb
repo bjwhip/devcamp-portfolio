bbd767b290f42dfbc690850e16575bde6b057949
bjwhip <bjwhip@gmail.com>
2017-08-29 14:15:40 -0600

    Added Custom Scopes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,16 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
+#ruby page
+  def ruby_on_rails
+    @ruby_on_rails_portfolio_items = Portfolio.ruby_on_rails
+  end
+#ruby page
+
   def new
     @portfolio_item = Portfolio.new
   end
