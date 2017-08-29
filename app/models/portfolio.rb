618cd1ebb656754d7e7e904ad3f6e4a5dd0b9e28
bjwhip <bjwhip@gmail.com>
2017-08-29 15:02:37 -0600

    Create concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,6 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
-
-  after_initialize :set_defaults
 
 #Custom Scopes------------------------------------------------------------
   def self.angular
@@ -11,8 +10,10 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 #end custom Scopes--------------------------------------------------------
 
+  after_initialize :set_defaults
+
   def set_defaults
-    self.main_image ||= "https://hzboyw.dm2301.livefilestore.com/y4mC9zX_IYHebgupreRjPN-YKwPg9b39g_6gAmC3VG4rNHfQb7Llf9rLInRJMTtXGEcVsH83pMDL34o-y05eTa5sW9st6typHNJ4nTky1MU8F28gxLfcbafPPDnn3gSeV2mO-U3Me0vP0tzDrP8w7665huxBtu78iO9eEcAVbY7SmS9mK5KVXgUlAwhBH6TC51_MXIdxGKbW-Sz11Po5iUJaA?width=1024&height=392&cropmode=none"
-    self.thumb_image ||= "https://hzboyw.dm2301.livefilestore.com/y4mC9zX_IYHebgupreRjPN-YKwPg9b39g_6gAmC3VG4rNHfQb7Llf9rLInRJMTtXGEcVsH83pMDL34o-y05eTa5sW9st6typHNJ4nTky1MU8F28gxLfcbafPPDnn3gSeV2mO-U3Me0vP0tzDrP8w7665huxBtu78iO9eEcAVbY7SmS9mK5KVXgUlAwhBH6TC51_MXIdxGKbW-Sz11Po5iUJaA?width=660&height=252&cropmode=none"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
