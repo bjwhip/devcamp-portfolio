4706f3f9ada7e6643e36192a6dc5385e0098ff85
bjwhip <bjwhip@gmail.com>
2017-08-29 14:38:58 -0600

    Set data defaults for Portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,8 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  after_initialize :set_defaults
+
 #Custom Scopes------------------------------------------------------------
   def self.angular
     where(subtitle: 'Angular')
@@ -9,4 +11,8 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 #end custom Scopes--------------------------------------------------------
 
+  def set_defaults
+    self.main_image ||= "https://hzboyw.dm2301.livefilestore.com/y4mC9zX_IYHebgupreRjPN-YKwPg9b39g_6gAmC3VG4rNHfQb7Llf9rLInRJMTtXGEcVsH83pMDL34o-y05eTa5sW9st6typHNJ4nTky1MU8F28gxLfcbafPPDnn3gSeV2mO-U3Me0vP0tzDrP8w7665huxBtu78iO9eEcAVbY7SmS9mK5KVXgUlAwhBH6TC51_MXIdxGKbW-Sz11Po5iUJaA?width=1024&height=392&cropmode=none"
+    self.thumb_image ||= "https://hzboyw.dm2301.livefilestore.com/y4mC9zX_IYHebgupreRjPN-YKwPg9b39g_6gAmC3VG4rNHfQb7Llf9rLInRJMTtXGEcVsH83pMDL34o-y05eTa5sW9st6typHNJ4nTky1MU8F28gxLfcbafPPDnn3gSeV2mO-U3Me0vP0tzDrP8w7665huxBtu78iO9eEcAVbY7SmS9mK5KVXgUlAwhBH6TC51_MXIdxGKbW-Sz11Po5iUJaA?width=660&height=252&cropmode=none"
+  end
 end
