2b04a83781426eb9bddac9d9076d3b62790c67c2
bjwhip <bjwhip@gmail.com>
2017-08-31 12:47:09 -0600

    changed white list to be more elegant

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,7 +19,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -39,7 +39,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The Portfolio was successfully updated.' }
       else
         format.html { render :edit }
@@ -63,6 +63,12 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url notice: 'Record was successfully Deleted.' }
     end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 
 end
