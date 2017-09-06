d95c1aaf93b97cfc23662ba88c1b9ba35204a8ad
bjwhip <bjwhip@gmail.com>
2017-09-06 15:34:32 -0600

    Updated master layout styles, helper methods and created footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,13 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser)
-      (link_to "Sign Up", new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to "Login", new_user_session_path)
+      (link_to "Sign Up", new_user_registration_path, class: style) +
+
+      (link_to "Login", new_user_session_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
-
-  # def source_helper
-  #   if session[:source]
-  #     greeting = "I like your photos from #{session[:source] current_user.first_name if current_user}"
-  #     content_tag(:p, greeting, class= "source-greeting")
-  #   end
-  # end
 
   def source_helper
     if session[:source]
