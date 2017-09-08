3acaf009751729ea84970810a61a2bf82cdc55e8
bjwhip <bjwhip@gmail.com>
2017-09-08 15:34:24 -0600

    Revert "integrated initial drag and drop implementation"
    This reverts commit ba61488940652a946eb6899b3dcc96dd2aa2e9be.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require bootstrap-sprockets
-//= require jquery_ujs
+//= require rails-ujs
 //= require jquery-ui
-//= require html.sortable
+//= html.sortable
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
