8b2c2612373b70db0ff477b9fe399740d2f9f566
bjwhip <bjwhip@gmail.com>
2017-09-26 15:16:58 -0600

    Somehow created awesome Dr. Who paralax effect

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,7 +72,7 @@ module ApplicationHelper
   end
 
   def alert_generator msg
-    js add_gritter(msg, title: "EEEEE", time: 600, sticky: false)
+    js add_gritter(msg, title: "One Message", time: 600, sticky: false)
   end
 
 end
